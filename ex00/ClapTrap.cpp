d16171e42caa783a173f2a97162176aa47801151
Juiceee <lbehr@student.42.fr>
2024-05-22 16:51:39 +0200

    oui

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -1,6 +1,6 @@
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap() : _name("ClapTrap"), _hitPoints(10), _energyPoints(10), _attaqueDamage(0)
+ClapTrap::ClapTrap() : _name("Default"), _hitPoints(10), _energyPoints(10), _attaqueDamage(0)
 {
 	std::cout << "ClapTrap Default constructor called\n";
 }
